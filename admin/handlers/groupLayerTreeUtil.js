2d03f4729afbb4a1d718ad86d598935a1a01439c
Lukas Bergliden <lukas.bergliden@decerno.se>
2018-04-09 11:51:11 +0200

    Checked in missed file

---
+++ admin/handlers/groupLayerTreeUtil.js
@@ -0,0 +1,78 @@
+"use strict";
+const reconstructGroupLayerTree = (groups, layers) => {
+    let treeRoot = {groupId: 0, name: 'ROOT', children:[], object: {}};   
+    treeRoot = addGroupsAndLayersToGroup(treeRoot, groups, layers);
+    return treeRoot;
+}
+
+const addGroupsAndLayersToGroup = (currentTreeNode, groups, layers) => {
+    groups.forEach(function(group){
+        if(!group.name){
+            console.log("Group with name is null found");
+        }
+        else{
+            if((group.parent == null && currentTreeNode.name == "ROOT") || (group.parent == currentTreeNode.name)){
+                let newGroupNode = {groupId: group.id, name: group.name, object: group, parent:group.parent, parentId: group.parent, children:[],order_number: group.order_number};   
+                if(groups.length > 0 ||layers.length > 0){
+                    newGroupNode = addGroupsAndLayersToGroup(newGroupNode, groups, layers);         
+                }
+                currentTreeNode.children.push(newGroupNode);
+            }
+            else{
+                layers.forEach(function(layer){
+                    if(layer.group_id == currentTreeNode.groupId){
+                        let newLayerNode = {layerId: layer.id, name: layer.title, object:layer, parentId: layer.group_id, leaf:true, order_number: layer.order_number};   
+                        currentTreeNode.children.push(newLayerNode);
+                        remove(layers, layer);
+                    }
+                });
+            }
+        }
+    });
+    currentTreeNode.children.sort(sortGroupOrLayerFunction);
+    return currentTreeNode; 
+}
+
+function sortGroupOrLayerFunction(node1, node2){
+    if(node1.order_number < node2.order_number){
+        return -1;
+    }
+    if(node1.order_number > node2.order_number){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+function splitLayerGroupTreeIntoOrderedLists(currentNode, result){
+    if(currentNode.children){
+        currentNode.object.groups = [];
+        currentNode.children.sort(sortGroupOrLayerFunction).forEach(function(item){
+            if(item.groupId){
+                currentNode.object.groups.push(item.object.createJsonObject());
+                result.addedGroups.push(item); //mark as added so we dont get dupplicates
+            }
+            else{
+                item.object.group = currentNode.object;
+                result.layers.push(item.object);
+            }
+            result = splitLayerGroupTreeIntoOrderedLists(item, result);
+        });
+    }
+    if(currentNode.groupId && currentNode.parent == null){
+        result.groups.push(currentNode.object.createJsonObject());
+    }
+    else if(currentNode.layerId){
+        //result.layers.push(currentNode.object);
+    }
+    return result;
+}
+
+function remove(array, element) {
+    const index = array.indexOf(element);
+    array.splice(index, 1);
+}
+   
+module.exports.reconstructGroupLayerTree = reconstructGroupLayerTree;
+module.exports.splitLayerGroupTreeIntoOrderedLists = splitLayerGroupTreeIntoOrderedLists;
