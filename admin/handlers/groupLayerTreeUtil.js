ccc8106e3503523b8a40df17ca8194a42a26bd72
Lukas Bergliden <lukas.bergliden@decerno.se>
2018-04-16 07:14:59 +0200

    Fixed syntax format

--- admin/handlers/groupLayerTreeUtil.js
+++ admin/handlers/groupLayerTreeUtil.js
@@ -1,27 +1,27 @@
 "use strict";
 const reconstructGroupLayerTree = (groups, layers) => {
-    let treeRoot = {groupId: 0, title: "", name: 'ROOT', children:[], object: {}};   
+    let treeRoot = { groupId: 0, title: "", name: 'ROOT', children: [], object: {} };
     treeRoot = addGroupsAndLayersToGroup(treeRoot, groups, layers);
     return treeRoot;
 }
 
 const addGroupsAndLayersToGroup = (currentTreeNode, groups, layers) => {
-    groups.forEach(function(group){
-        if(!group.name){
+    groups.forEach(function (group) {
+        if (!group.name) {
             console.log("Group with name is null found");
         }
-        else{
-            if((group.parent == null && currentTreeNode.name == "ROOT") || (group.parent == currentTreeNode.name)){
-                let newGroupNode = {groupId: group.id, name: group.name, title: group.title, object: group, parent:group.parent, parentId: group.parent, children:[],order_number: group.order_number};   
-                if(groups.length > 0 ||layers.length > 0){
-                    newGroupNode = addGroupsAndLayersToGroup(newGroupNode, groups, layers);         
+        else {
+            if ((group.parent == null && currentTreeNode.name == "ROOT") || (group.parent == currentTreeNode.name)) {
+                let newGroupNode = { groupId: group.id, name: group.name, title: group.title, object: group, parent: group.parent, parentId: group.parent, children: [], order_number: group.order_number };
+                if (groups.length > 0 || layers.length > 0) {
+                    newGroupNode = addGroupsAndLayersToGroup(newGroupNode, groups, layers);
                 }
                 currentTreeNode.children.push(newGroupNode);
             }
-            else{
-                layers.forEach(function(layer){
-                    if(layer.group_id == currentTreeNode.groupId){
-                        let newLayerNode = {layerId: layer.id, name: layer.name, title:layer.title, object:layer, parentId: layer.group_id, leaf:true, order_number: layer.order_number};   
+            else {
+                layers.forEach(function (layer) {
+                    if (layer.group_id == currentTreeNode.groupId) {
+                        let newLayerNode = { layerId: layer.id, name: layer.name, title: layer.title, object: layer, parentId: layer.group_id, leaf: true, order_number: layer.order_number };
                         currentTreeNode.children.push(newLayerNode);
                         remove(layers, layer);
                     }
@@ -30,42 +30,42 @@ const addGroupsAndLayersToGroup = (currentTreeNode, groups, layers) => {
         }
     });
     currentTreeNode.children.sort(sortGroupOrLayerFunction);
-    return currentTreeNode; 
+    return currentTreeNode;
 }
 
-function sortGroupOrLayerFunction(node1, node2){
-    if(node1.order_number < node2.order_number){
+function sortGroupOrLayerFunction(node1, node2) {
+    if (node1.order_number < node2.order_number) {
         return -1;
     }
-    if(node1.order_number > node2.order_number){
+    if (node1.order_number > node2.order_number) {
         return 1;
     }
-    else{
+    else {
         return 0;
     }
 }
 
-function splitLayerGroupTreeIntoOrderedLists(currentNode, result){
-    if(currentNode.children){
+function splitLayerGroupTreeIntoOrderedLists(currentNode, result) {
+    if (currentNode.children) {
         currentNode.object.groups = [];
-        currentNode.children.sort(sortGroupOrLayerFunction).forEach(function(item){
-            if(item.groupId){
+        currentNode.children.sort(sortGroupOrLayerFunction).forEach(function (item) {
+            if (item.groupId) {
                 result.addedGroups.push(item); //mark as added so we dont get dupplicates
                 let resultSubCall = splitLayerGroupTreeIntoOrderedLists(item, result);
                 result.layers.concat(resultSubCall.layers);
                 result.groups.concat(resultSubCall.groups);
                 currentNode.object.groups.push(item.object.createJsonObject());
             }
-            else{
+            else {
                 item.object.group = currentNode.object;
                 result.layers.push(item.object);
             }
         });
     }
-    if(currentNode.groupId && currentNode.parent == null){
+    if (currentNode.groupId && currentNode.parent == null) {
         result.groups.push(currentNode.object.createJsonObject());
     }
-    else if(currentNode.layerId){
+    else if (currentNode.layerId) {
         //result.layers.push(currentNode.object);
     }
     return result;
@@ -75,6 +75,6 @@ function remove(array, element) {
     const index = array.indexOf(element);
     array.splice(index, 1);
 }
-   
+
 module.exports.reconstructGroupLayerTree = reconstructGroupLayerTree;
 module.exports.splitLayerGroupTreeIntoOrderedLists = splitLayerGroupTreeIntoOrderedLists;
